9d4c178c270d897f11193ca14d22332ffc60dab0
dbruno2 <dbruno2@studenti.uninsubria.it>
2025-05-05 22:37:32 +0200

    modifiche generali
    Co-Authored-By: ssvezia <206748203+ssvezia@users.noreply.github.com>

--- dto/ristorante.java
+++ dto/ristorante.java
@@ -1,15 +1,71 @@
 package dto;
 
-import java.util.Calendar;
+public class Ristorante{
 
-public class ristorante{
-    private ENUM Nazione;
+    private String Nazione;
     private String  Città;
     private String Indirizzo;
     private int Latitudine;
     private int  Longitudine;
     private int  prezzo;
     private boolean Disponibilità_delivery;
-    private boolean Disponibilità_delivery;
-    private ENUM Tipo_Cucina;
+    private boolean Disponibilità_prenotazione;
+    private String Tipo_Cucina;
+    
+    
+	public String getNazione() {
+		return Nazione;
+	}
+	public void setNazione(String nazione) {
+		Nazione = nazione;
+	}
+	public String getCittà() {
+		return Città;
+	}
+	public void setCittà(String città) {
+		Città = città;
+	}
+	public String getIndirizzo() {
+		return Indirizzo;
+	}
+	public void setIndirizzo(String indirizzo) {
+		Indirizzo = indirizzo;
+	}
+	public int getLatitudine() {
+		return Latitudine;
+	}
+	public void setLatitudine(int latitudine) {
+		Latitudine = latitudine;
+	}
+	public int getLongitudine() {
+		return Longitudine;
+	}
+	public void setLongitudine(int longitudine) {
+		Longitudine = longitudine;
+	}
+	public int getPrezzo() {
+		return prezzo;
+	}
+	public void setPrezzo(int prezzo) {
+		this.prezzo = prezzo;
+	}
+	public boolean isDisponibilità_delivery() {
+		return Disponibilità_delivery;
+	}
+	public void setDisponibilità_delivery(boolean disponibilità_delivery) {
+		Disponibilità_delivery = disponibilità_delivery;
+	}
+	public boolean isDisponibilità_prenotazione() {
+		return Disponibilità_prenotazione;
+	}
+	public void setDisponibilità_prenotazione(boolean disponibilità_prenotazione) {
+		Disponibilità_prenotazione = disponibilità_prenotazione;
+	}
+	public String getTipo_Cucina() {
+		return Tipo_Cucina;
+	}
+	public void setTipo_Cucina(String tipo_Cucina) {
+		Tipo_Cucina = tipo_Cucina;
+	}
+    
 }
--- dto/utente.java
+++ dto/utente.java
@@ -13,4 +13,55 @@ public class Utente {
 	 private String ruolo;
 	 private Ristorante[] preferiti;
 
+     public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public String getCognome() {
+		return Cognome;
+	}
+	public void setCognome(String cognome) {
+		Cognome = cognome;
+	}
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
+	}
+	public Calendar getData_nascita() {
+		return Data_nascita;
+	}
+	public void setData_nascita(Calendar data_nascita) {
+		Data_nascita = data_nascita;
+	}
+	public String getLuogo_domicilio() {
+		return Luogo_domicilio;
+	}
+	public void setLuogo_domicilio(String luogo_domicilio) {
+		Luogo_domicilio = luogo_domicilio;
+	}
+	public String getRuolo() {
+		return ruolo;
+	}
+	public void setRuolo(String ruolo) {
+		this.ruolo = ruolo;
+	}
+	public Ristorante[] getPreferiti() {
+		return preferiti;
+	}
+	public void setPreferiti(Ristorante[] preferiti) {
+		this.preferiti = preferiti;
+	}
+	 
+	 
+
 }
