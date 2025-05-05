8aec5ae1cedb5d7615da5c66167abf3f854cb640
ssvezia <ssvezia@studenti.uninsubria.it>
2025-05-05 22:33:36 +0200

    creato file recensioni e modifica variabili ENUM a Stringa
    Co-Authored-By: dbruno2 <206684825+dbruno2@users.noreply.github.com>

---
+++ dto/recensioni.java
@@ -0,0 +1,11 @@
+package dto;
+
+import java.util.Calendar;
+
+public class preferiti{
+    private String Username;
+    private Ristorante[] nome;
+    private int valutazione;
+    private String testo;
+    private String risposta;
+}
