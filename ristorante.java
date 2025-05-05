457abbec29648a53815201577b0a2be9749b100c
ssvezia <ssvezia@studenti.uninsubria.it>
2025-05-05 22:19:21 +0200

    Create ristorante.java

---
+++ ristorante.java
@@ -0,0 +1,13 @@
+import java.util.Calendar;
+
+public class ristorante{
+    private ENUM Nazione;
+    private String  Città;
+    private String Indirizzo;
+    private int Latitudine;
+    private int  Longitudine;
+    private int  prezzo;
+    private boolean Disponibilità_delivery;
+    private boolean Disponibilità_delivery;
+    private ENUM Tipo_Cucina;
+}
