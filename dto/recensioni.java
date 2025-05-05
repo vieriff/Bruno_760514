10862b100a3254ab701507cc20e70e40d2efc94e
dbruno2 <dbruno2@studenti.uninsubria.it>
2025-05-05 22:46:12 +0200

    classe recensioni
    Co-Authored-By: ssvezia <206748203+ssvezia@users.noreply.github.com>

--- dto/recensioni.java
+++ dto/recensioni.java
@@ -2,10 +2,44 @@ package dto;
 
 import java.util.Calendar;
 
-public class preferiti{
+public class Recensioni{
+
     private String Username;
-    private Ristorante[] nome;
+    private String nomeRistorante;
     private int valutazione;
     private String testo;
     private String risposta;
+    
+    
+    public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	public String getNomeRistorante() {
+		return nomeRistorante;
+	}
+	public void setNomeRistorante(String nomeRistorante) {
+		this.nomeRistorante = nomeRistorante;
+	}
+	public int getValutazione() {
+		return valutazione;
+	}
+	public void setValutazione(int valutazione) {
+		this.valutazione = valutazione;
+	}
+	public String getTesto() {
+		return testo;
+	}
+	public void setTesto(String testo) {
+		this.testo = testo;
+	}
+	public String getRisposta() {
+		return risposta;
+	}
+	public void setRisposta(String risposta) {
+		this.risposta = risposta;
+	}
+
 }
