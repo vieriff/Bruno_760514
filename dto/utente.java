84b63ab2bf752cc8c1ae73717cfff478bdb51266
dbruno2 <dbruno2@studenti.uninsubria.it>
2025-05-05 22:20:03 +0200

    classe utente

---
+++ dto/utente.java
@@ -0,0 +1,16 @@
+package dto;
+
+import java.util.Calendar;
+
+public class Utente {
+	
+	 private String Nome;
+	 private String Cognome;
+	 private String Username;
+	 private String Password;
+	 private Calendar Data_nascita;
+	 private String Luogo_domicilio;
+	 private String ruolo;
+	 private Ristorante[] preferiti;
+
+}
