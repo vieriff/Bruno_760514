607383ab8c4b116281f83b189fe7cafc085c808f
dbruno2 <dbruno2@studenti.uninsubria.it>
2025-05-06 21:55:56 +0200

    classe mapper
    Co-Authored-By: ssvezia <206748203+ssvezia@users.noreply.github.com>

---
+++ mapper/Mapper.java
@@ -0,0 +1,15 @@
+package mapper;
+
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import dto.Biglietto;
+import dto.Evento;
+
+public class Mapper {
+
+   
+  
+}
